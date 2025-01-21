4cf955157469a29e4a0b4be4bf0c9a7d9bb9a6bc
RenatoSantos7 <renatodisantos@outlook.com>
2025-01-21 00:28:58 -0300

    more updates

--- nodes.py
+++ nodes.py
@@ -1,16 +1,18 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from ..util import AwsConfig
+from typing import Any, Dict
+from ..util import AwsConfig, Status, State
 import awswrangler as wr
-import logging
+import logger
 
-logging.basicConfig(level=logging.INFO)
+
+logger = logger.getLogger(__name__)
 
 
 def _create_table(config):
     """
     This is a wrap function to awswrangler.catalog.create_parquet_table.
     """
-    logging.debug(f"Creating table {config.table}.")
+    logger.debug(f"Creating table {config.table}.")
     try:
         wr.catalog.create_parquet_table(
             table=config.table,
@@ -23,7 +25,7 @@ def _create_table(config):
             mode=config.mode,
         )
     except Exception as exc:
-        logging.error(
+        logger.error(
             f"An exception occurred while creating table {config.table}. Exception: {exc}, Type: {type(exc)}"
         )
         raise
@@ -33,7 +35,7 @@ def _unload_partition(query, query_id, config):
     """
     This is a wrap function to awswrangler.athena.unload
     """
-    logging.debug(f"Starting UNLOAD query {query_id}:\n{query}")
+    logger.debug(f"Starting UNLOAD query {query_id}:\n{query}")
     try:
         query_metadata = wr.athena.unload(
             sql=query,
@@ -46,10 +48,10 @@ def _unload_partition(query, query_id, config):
             workgroup=config.workgroup,
         )
     except Exception as err:
-        logging.error(f"Unexpected {err=}, {type(err)=}")
+        logger.error(f"Unexpected {err=}, {type(err)=}")
         return None
     else:
-        logging.debug(f"Query {query_id} completed.\nQuery metadata:\n{query_metadata}")
+        logger.debug(f"Query {query_id} completed.\nQuery metadata:\n{query_metadata}")
         return query_metadata
 
 
@@ -68,26 +70,28 @@ def _execute_unload_partitions(query, periods, config):
             )
             future_to_query[future] = (formatted_query, query_id)
 
+        results = {}
         for future in as_completed(future_to_query):
             formatted_query, query_id = future_to_query[future]
             try:
                 result = future.result()
             except Exception as e:
-                logging.error(f"Query {query_id} generated an exception: {e}")
+                logger.error(f"Query {query_id} generated an exception: {e}")
+                results[query_id] = "FAIL"
             else:
+                results[query_id] = result
                 if result is not None:
-                    logging.info(
-                        f"Query {query_id} succeeded. Query metadata: {result}"
-                    )
+                    logger.info(f"Query {query_id} succeeded. Query metadata: {result}")
                 else:
-                    logging.warning(f"Query {query_id} did not produced any results.")
+                    logger.warning(f"Query {query_id} did not produced any results.")
+        return results
 
 
 def _repair_table(config):
     """
     This is a wrap function to awswrangler.athena.repair_table
     """
-    logging.info(f"Repairing table {config.table} in database {config.database}.")
+    logger.info(f"Repairing table {config.table} in database {config.database}.")
     try:
         state = wr.athena.repair_table(
             table=config.table,
@@ -97,41 +101,44 @@ def _repair_table(config):
             workgroup=config.workgroup,
         )
     except Exception as exc:
-        logging.error(
+        logger.error(
             f"An exception occurred while repairing table {config.table}. Exception: {exc}, Type: {type(exc)}"
         )
         raise
     else:
         if state == "SUCCEEDED":
-            logging.info(f"MSCK on table {config.table} succeeded.")
+            logger.info(f"MSCK on table {config.table} succeeded.")
         elif state == "FAIL":
-            logging.error(f"MSCK on table {config.table} failed.")
+            logger.error(f"MSCK on table {config.table} failed.")
         elif state == "CANCELLED":
-            logging.warning(f"MSCK on table {config.table} was cancelled.")
+            logger.warning(f"MSCK on table {config.table} was cancelled.")
         else:
-            logging.error(
+            logger.error(
                 f"MSCK on table {config.table} resulted in an unknown state: {state}."
             )
             raise ValueError(
                 f"Unknown state: {state} encountered during MSCK on table {config.table}."
             )
+    return state
 
 
-def build_table(table_params):
+def build_table(table_params: AwsConfig) -> Dict[str, Any]:
     """
     Builds a table given table parameters.
     """
     config = AwsConfig(table_params)
-    logging.info(f"Building table {config.table} with params:\n {table_params}.")
+    logger.info(f"Building table {config.table} with params:\n {table_params}.")
+    build_status = Status(State.SUCCESS)
     try:
         _create_table(config)
         query = config.sql
         periods = list(table_params["periods"].values())
-        _execute_unload_partitions(query, periods, config)
+        unload_results = _execute_unload_partitions(query, periods, config)
         _repair_table(config)
     except Exception as exc:
-        logging.error(
+        logger.error(
             f"An exception occurred while building table {config.table}. Exception: {exc}, Type: {type(exc)}"
         )
-        raise
-    logging.info(f"Table {config.table} created.")
+        build_status.state = State.FAIL
+    logger.info(f"Table {config.table} created.")
+    return build_status, unload_results
--- util.py
+++ util.py
@@ -1,5 +1,28 @@
 from dataclasses import dataclass, field
 from typing import Optional, Dict, List, Literal
+from enum import Enum
+
+
+class State(Enum):
+    SUCCESS = "success"
+    FAIL = "fail"
+
+class Status:
+    def __init__(self, initial_state: State):
+        if not isinstance(initial_state, State):
+            raise ValueError("Initial state must be a valid State (SUCCESS or FAIL)")
+        self._state = initial_state
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, new_state: State):
+        if not isinstance(new_state, State):
+            raise ValueError("New state must be a valid State (SUCCESS or FAIL)")
+        self._state = new_state
+
 
 @dataclass
 class AwsConfig:
@@ -21,6 +44,7 @@ class AwsConfig:
         mode (Literal['overwrite', 'append']): Write mode for table operations.
         workgroup (Optional[str]): Workgroup to use for Athena queries.
     """
+
     sql: str
     table: str
     database: str
@@ -32,7 +56,7 @@ class AwsConfig:
     compression: Optional[str] = field(default="snappy")
     data_source: Optional[str] = None
     description: Optional[str] = None
-    mode: Literal['overwrite', 'append'] = field(default="overwrite")
+    mode: Literal["overwrite", "append"] = field(default="overwrite")
     workgroup: Optional[str] = None
 
     def __post_init__(self):
@@ -41,34 +65,43 @@ class AwsConfig:
         """
         valid_file_formats = {"parquet", "csv", "json"}
         valid_compressions = {"snappy", "gzip", "bzip2", None}
-        
+
         if self.file_format not in valid_file_formats:
-            raise ValueError(f"Invalid file_format '{self.file_format}'. Must be one of {valid_file_formats}.")
-        
+            raise ValueError(
+                f"Invalid file_format '{self.file_format}'. Must be one of {valid_file_formats}."
+            )
+
         if self.compression not in valid_compressions:
-            raise ValueError(f"Invalid compression '{self.compression}'. Must be one of {valid_compressions}.")
+            raise ValueError(
+                f"Invalid compression '{self.compression}'. Must be one of {valid_compressions}."
+            )
 
         if self.mode not in {"overwrite", "append"}:
-            raise ValueError(f"Invalid mode '{self.mode}'. Must be 'overwrite' or 'append'.")
+            raise ValueError(
+                f"Invalid mode '{self.mode}'. Must be 'overwrite' or 'append'."
+            )
 
         if self.partitioned_by and not self.partitions_types:
-            raise ValueError("If 'partitioned_by' is specified, 'partitions_types' must also be provided.")
+            raise ValueError(
+                "If 'partitioned_by' is specified, 'partitions_types' must also be provided."
+            )
 
         if not self.path.startswith("s3://"):
             raise ValueError(f"Invalid path '{self.path}'. Must start with 's3://'.")
 
+
 def build_aws_config(params):
     return AwsConfig(
-            sql=params['select_query'],
-            table=params['table'],
-            database=params['database'],
-            path=params['location'],
-            columns_types=params['columns_types'],
-            partitions_types=params['partitions_cols'],
-            partitioned_by=params['partitioned_by'],
-            description=params['description'],
-            file_format=params['file_format'],
-            compression=params['compression'],
-            data_source=params['data_source'],
-            workgroup=params['workgroup']
-            )
+        sql=params["select_query"],
+        table=params["table"],
+        database=params["database"],
+        path=params["location"],
+        columns_types=params["columns_types"],
+        partitions_types=params["partitions_cols"],
+        partitioned_by=params["partitioned_by"],
+        description=params["description"],
+        file_format=params["file_format"],
+        compression=params["compression"],
+        data_source=params["data_source"],
+        workgroup=params["workgroup"],
+    )
