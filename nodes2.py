e087665dd584c101bd4ade5909143fd6a9d95823
RenatoSantos7 <renatodisantos@outlook.com>
2025-01-22 01:32:23 -0300

    more updates

--- nodes2.py
+++ nodes2.py
@@ -9,14 +9,14 @@ def _check_primary_datasets(table_statuses: Dict[str, Status]) -> Status:
     """
     Check if primary datasets were built successfully.
     """
-    check_status = Status.OK
+    check_status = Status(State.OK)
     has_failed = []
     for table, build in table_statuses.items():
         if build.state is State.FAIL:
             logger.error(f"Table {table} failed to be built.")
             has_failed.append(table)
     if has_failed:
-        check_status = Status.FAILED
+        check_status.state = State.FAILED
     return check_status
 
 
@@ -24,11 +24,11 @@ def build_abt(config: AwsConfig, **table_statuses) -> Tuple[Dict[str, Status], A
     """
     Construct the Analytical Base Table from primary datasets.
     """
-    _build_abt = Status.SUCCESS
+    _build_abt = State.SUCCESS
     logger.info("Checking states of primary datasets.") 
     datasets_state = _check_primary_datasets(table_statuses).state
     if datasets_state is not State.SUCCESS:
         logger.error("Some primary datasets failed to be build.")
-        _build_abt = Status.FAIL
+        _build_abt = State.FAIL
         return {config.table: _build_abt}, None
 
